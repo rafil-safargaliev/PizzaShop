749d96a07eaa22d1163f2e0ed84bb444e9972c84
rafil <rafilsafargaliev@gmail.com>
2024-02-06 16:05:56 +0300

    pass id to add_to_cart

--- public/app.js
+++ public/app.js
@@ -10,9 +10,9 @@ function something()
     alert(x);
 }
 
-function add_to_cart()
+function add_to_cart(id)
 {
 
-    alert("Hello from function!");
+    alert('You added pizza with id: ' + id);
 
 }
