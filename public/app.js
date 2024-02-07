7050019bcd13a4b695400d9e098c7a9bdf8dae2a
rafil <rafilsafargaliev@gmail.com>
2024-02-07 10:53:56 +0300

    update orders button

--- public/app.js
+++ public/app.js
@@ -17,14 +17,21 @@ function add_to_cart(id)
     var x = window.localStorage.getItem(key);
     x = x *1 + 1;
     window.localStorage.setItem(key, x);
-    
+
     update_orders_input();
+    update_orders_button();
 }
 
 function update_orders_input()
     {
         var orders = cart_get_orders();
         $('#orders_input').val(orders);
+    }
+
+    function update_orders_button()
+    {
+        var text = Cart ('+ cart_get_number_of_iems() + ')
+        $('#orders_button').val(text)
     }
 
 function cart_get_number_of_items()
